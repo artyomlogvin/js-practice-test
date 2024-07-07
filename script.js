332cbbd4a5f44b5c0fa24ee6ed047a74febdc98f
Artyom <43935362+artyomlogvin@users.noreply.github.com>
2024-07-07 15:13:26 +0300

    Implement adding pictures every 3 secs logic

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const img = document.querySelector("img");
-fetch("https://dog.ceo/api/breeds/image/random", {
-  mode: "cors",
-})
-  .then(function (response) {
-    return response.json();
+const container = document.querySelector(".pic-div");
+
+function getImage() {
+  fetch("https://dog.ceo/api/breeds/image/random", {
+    mode: "cors",
   })
-  .then(function (response) {
-    console.log(response);
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      let newImg = document.createElement("img");
+      container.appendChild(newImg);
+      let images = document.querySelectorAll("img");
+      images[images.length - 1].src = response.message;
+    });
+}
+
+function addImages() {
+  getImage();
+
+  setTimeout(addImages, 3000);
+}
+
+addImages();
